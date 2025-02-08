431f740c0a778f971672733a4695c5731a46359f
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-02-08 20:10:05 +0300

    fix: relocated the navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppLayoutComponent } from "./app-layout/app-layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, AppLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
