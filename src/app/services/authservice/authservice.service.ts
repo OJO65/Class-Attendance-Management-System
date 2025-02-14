0036b9dfee92d2c1eb70daa437705e7e2bdcc3b8
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-02-14 21:13:55 +0300

    feat: authservice for login

--- src/app/services/authservice/authservice.service.ts
+++ src/app/services/authservice/authservice.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthserviceService {
+private apiUrl = 'http://localhost:5000/auth'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  login(adm_no: string, password: string): Observable<any>{
+    return this.http.post(`${this.apiUrl}/login`, {adm_no, password});
+  }
 }
