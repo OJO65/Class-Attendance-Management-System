c6af0d7e3ba66ceb5580fb0c385d0f14499fe5ae
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-02-14 21:13:25 +0300

    feat: incoperating backend to login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,23 +1,34 @@
 import { NgClass, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { AuthserviceService } from '../../services/authservice/authservice.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, NgIf, NgClass, RouterModule],
+  imports: [FormsModule, NgIf, NgClass, RouterModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  authService = inject(AuthserviceService);
+  router = inject(Router);
 
   onSubmit(form: NgForm) {
     if (form.valid) {
-      console.log('Form Submitted', form.value);
-      //redirect to home
-      this.router.navigate(['/home']);
+      console.log('Logging in with:', form.value);
+      this.authService.login(form.value.adm_no, form.value.password).subscribe({
+        next: (response) => {
+          console.log('Login successful:', response)
+          //redirect to home
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          console.error('Login failed:', err);
+        }
+      });
     } else {
       console.log('Form is invalid');
     }
