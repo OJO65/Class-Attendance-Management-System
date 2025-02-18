3b68b3c1797cc582d30abcfce87f804bb6cf16aa
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-02-18 11:08:03 +0300

    feat: updated query

--- backend/table.sql
+++ backend/table.sql
@@ -9,40 +9,45 @@ create table
     );
 
 insert into
-    users (name, teacher_id, password, role)
+    users (name, adm_no, teacher_id, password, role)
 values
-    ('kasongo', 'MA_17', '123456', 'teacher');
+    ('kasongo', NULL, 'MA_17', '123456', 'teacher');
 
 insert into
-    users (name, adm_no, password, role)
+    users (name, adm_no, teacher_id, password, role)
 values
     (
         'jeffrey',
         'TKNP/B/6030',
+        NULL,
         'hashedpassword1',
         'student'
     ),
     (
         'Emily Ogolla',
         'TKNP/B/5775',
+        NULL,
         'hashedpassword2',
         'student'
     ),
     (
         'Velma Odhiambo',
         'TKNP/B/6168',
+        NULL,
         'hashedpassword3',
         'student'
     ),
     (
         'Purity Euniva',
         'TKNP/B/5952',
+        NULL,
         'hashedpassword4',
         'student'
     ),
     (
         'Lenox Jobita',
         'TKNP/B/6040',
+        NULL,
         'hashedpassword5',
         'student'
     );
