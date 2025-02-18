4884709c339414579f439ee141b50f47d20963ce
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-02-18 11:07:44 +0300

    feat: debugging for user

--- backend/services/checkRole.js
+++ backend/services/checkRole.js
@@ -1,5 +1,8 @@
 function checkRole(requiredRole) {
     return (req, res, next) => {
+      console.log("user role from token:", res.locals.user?.role);
+      console.log("required role:", requiredRole);
+      
       if (!res.locals.user) {
         return res.status(401).json({ message: "Unauthorized access!" });
       }
