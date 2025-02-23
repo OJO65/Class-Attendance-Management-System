9e13e84b4d97f553532e7795dd6b249ec62cfcb2
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-02-23 19:38:04 +0300

    feat: student-dashboard component

--- src/app/pages/student-dashboard/student-dashboard.component.ts
+++ src/app/pages/student-dashboard/student-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
+
+import { Router } from '@angular/router';
 
 
 interface StudentAttendanceRecord {
@@ -11,32 +13,40 @@ interface StudentAttendanceRecord {
 
 @Component({
   selector: 'app-student-dashboard',
-  imports: [NgIf, NgClass, NgFor, DatePipe, HttpClientModule],
+  imports: [NgIf, NgClass, NgFor, DatePipe],
   templateUrl: './student-dashboard.component.html',
   styleUrl: './student-dashboard.component.css',
 })
 export class StudentDashboardComponent implements OnInit {
   attendanceData: StudentAttendanceRecord[] = [];
-  loading: boolean = false;
+  loadingAttendance: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
     this.fetchAttendanceData();
   }
 
   fetchAttendanceData() {
-    this.loading = true;
-    this.http.get<StudentAttendanceRecord[]>('http://localhost:8080/user/student-dashboard')
+    this.loadingAttendance = true;
+    this.http
+      .get<StudentAttendanceRecord[]>(
+        'http://localhost:8080/user/student-dashboard',
+        { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
+      )
       .subscribe({
         next: (data) => {
           this.attendanceData = data;
-          this.loading = false;
+          this.loadingAttendance = false;
         },
         error: (err) => {
-          console.error('Error fetching student dashboard data:', err);
-          this.loading = false;
+          console.error('Error fetching attendance data:', err);
+          this.loadingAttendance = false;
         },
       });
+  }
+
+  goToTimetable() {
+    this.router.navigate(['/timetable']);
   }
 }
