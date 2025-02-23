f6142e50d4d12f0a3e01ef04863c748614fc8336
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-02-23 19:37:16 +0300

    feat: logout logic

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  constructor(private authService: AuthService, private router: Router) {}
+  logout() {
+    this.authService.logout();
+  }
 }
